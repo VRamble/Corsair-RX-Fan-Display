bfb86f24406186efc13435eec55abdf3d2bef09a
bellh14 <haybell965@gmail.com>
2025-07-25 19:59:40 -0500

    feat: update makefile and add uint header

--- common/display_image.hpp
+++ common/display_image.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
-#include "display.hpp"
-
 #include <cstdint>
 #include <memory>
 
 typedef struct DisplayImage {
-    std::unique<uint8_t[]> r;
-    std::unique<uint8_t[]> g;
-    std::unique<uint8_t[]> b;
+    std::unique_ptr<uint8_t[]> r;
+    std::unique_ptr<uint8_t[]> g;
+    std::unique_ptr<uint8_t[]> b;
     uint8_t lineCount;   // number of lines sent at a time
     uint16_t pixelCount; // total number of pixels
 
